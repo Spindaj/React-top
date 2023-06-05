89cfeea2a6bf52615a35a17300dc126670292575
Spindaj <107429735+Spindaj@users.noreply.github.com>
2023-06-05 11:40:42 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ComponenteUm from "./components/ComponenteUm";
+import ComponenteDois from "./components/ComponenteDois";
+import ComponenteTres from "./components/ComponenteTres";
+
+function app(){
+    return(
+        <div>
+            <ComponenteUm />
+            <ComponenteDois />
+            <ComponenteTres />
+        </div>
+    )
+}
